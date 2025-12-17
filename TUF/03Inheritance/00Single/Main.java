e4eb2032125499759346e7e98e5b94c7690022c5
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2025-12-17 08:46:24 +0000

    single inheritance

---
+++ TUF/03Inheritance/00Single/Main.java
@@ -0,0 +1,32 @@
+import javax.xml.transform.stax.StAXResult;
+
+class School{
+    private String name;
+    School(){
+        name = "DPS";
+    }
+    void showSchoolName(){
+        System.out.println("School name is : " + name);
+    }
+}
+
+
+class Students extends School{
+    private String name;
+    Students(String name){
+        this.name = name;
+    }
+    void showStudentsName(){
+        System.out.println("Student name is : " + name);
+    }
+}
+
+public class Main {
+    public static void main(String[] args){
+        Students student1 = new Students("Hero");
+        student1.showStudentsName();
+        student1.showSchoolName();
+
+    }
+    
+}
