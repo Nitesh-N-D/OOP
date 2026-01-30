3b9a3da08488a112560a483c2301ef0bab61a020
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2026-01-30 05:19:17 +0000

    inner classc

---
+++ All Topics/08InnerClass/Main.java
@@ -0,0 +1,24 @@
+class Outerclass{
+    static int number = 10;
+    String name = "Outerclass";
+
+    static class Innerclass{
+        public void execute(){
+            System.out.println("Inside the inner class : ");
+        }
+    }
+    //rare use cases
+    // public void execute(){
+    //     Innerclass innerclass = new Innerclass();
+    //     innerclass.execute();
+    // }
+}
+
+
+public class Main {
+    public static void main(String[] args){
+        Outerclass.Innerclass innerclass1 = new Outerclass.Innerclass();
+        innerclass1.execute();
+    }
+    
+}
